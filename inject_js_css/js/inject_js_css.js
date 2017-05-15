284e33b93ff4b0804467c8b03c9a9891a7ccf635
Michael Kemp <michael@5kemps.com>
2017-05-15 17:17:17 -0600

    First Upload
    Inject jQuery or CSS into pages

---
+++ inject_js_css/js/inject_js_css.js
@@ -0,0 +1,4 @@
+(function ($) { jQuery(document).ready(function($) {
+    $("#edit-submit").addClass("btn btn-primary");
+    $("#edit-jquerypages-fieldset-insert-page").addClass("btn btn-success");
+}); }(jQuery));
